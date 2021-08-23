2b7df154492d8b44fbed13a3d824d60786bdc726
brahimjarallah <brahim.jarallah@gmail.com>
2021-08-23 19:14:57 +0100

    file-metadata-microservice-api

--- server.js
+++ server.js
@@ -1,20 +1,28 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+var express = require("express")
+var cors = require("cors")
+require("dotenv").config()
 
-var app = express();
+var app = express()
 
-app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use(cors())
+app.use("/public", express.static(process.cwd() + "/public"))
 
-app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
-});
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html")
+})
 
-
-
-
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
-});
+  console.log("Your app is listening on port " + port)
+})
+
+let multer = require("multer")
+
+app.post("/api/fileanalyse", multer().single("upfile"), (req, res) => {
+  console.log(req.file)
+  let respObj = {}
+  respObj["name"] = req.file.fieldname
+  respObj["type"] = req.file.mimetype
+  respObj["size"] = req.file.size
+  res.json(respObj)
+})
